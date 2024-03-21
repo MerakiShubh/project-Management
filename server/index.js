276b6028c4c2a9a8aa2a55262940a83d1eca3d07
MerakiShubh <shubhamt.champ@gmail.com>
2024-03-21 21:46:19 +0530

    package installation and project setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
 const express = require("express")
+require('dotenv').config();
+const port = process.env.PORT
+
+const app = express();
+
+app.listen(port, console.log(`Server is running is on port ${port}`))
